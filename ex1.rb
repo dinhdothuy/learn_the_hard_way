14f5f180c0ecae679017345ea17e5466f1a92b75
Daisy Dinh <daisydodinh@gmail.com>
2018-10-15 21:12:11 -0500

    Add ten cho

--- ex1.rb
+++ ex1.rb
@@ -6,7 +6,7 @@ puts "Yay! Printing."
 puts "I'd must rather you 'not'."
 puts 'I "said" do not touch this.'
 
-name = 'Thuy beo'
+name = 'Thuy beo Po Bunny Sunny Venus'
 
 puts "Hello, #{name}"
 puts 'Hello, #{name}'
